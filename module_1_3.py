c5f9069c372d11601e8bd9ef0d3285648c3544d6
Andrey Podianov <podyanov82@gmail.com>
2024-11-14 22:18:37 +0500

    Add files via upload
    Имя, возраст....

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+parametr = 'Name:'
+name = 'Андрей'
+print (parametr, name)
+parametr1 = 'Age:'
+age = 42
+print ((parametr1), (str(age)))
+parametr2 = 'New Age:'
+a = 15
+new_age = age+a
+print ((parametr2), (str(new_age)))
+parametr3 = 'Is student:'
+is_student = True
+print ((parametr3), (str(is_student)))
