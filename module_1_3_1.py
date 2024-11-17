e6b38d91e7a0d7f46aa04405cdad858b3b0e8836
Andrey Podianov <podyanov82@gmail.com>
2024-11-17 16:13:33 +0500

    Add files via upload
    Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.

---
+++ module_1_3_1.py
@@ -0,0 +1,5 @@
+homework_count = 12
+homework_hours_count = 1.5
+course_name = 'Python,'
+time_per_task = homework_hours_count / homework_count
+print('Курс:', course_name, 'всего задач:', str(homework_count)+',', 'затрачено часов:', str(homework_hours_count)+',', 'среднее время выполнения', time_per_task, 'часа.')
